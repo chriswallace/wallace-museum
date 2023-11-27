8801c7e4b88c1b418281fcc766b0a554dd78d6ec
Chris Wallace <chriswallace7@protonmail.com>
2023-11-27 14:01:11 -0600

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
+// vite.config.js
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
 
-export default defineConfig({
+/** @type {import('vite').UserConfig} */
+const config = {
 	plugins: [sveltekit()]
-});
+};
+
+export default config;
